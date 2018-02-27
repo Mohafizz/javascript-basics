6a346dce8291051f76a96d41a531a2cc2424f8d6
mohafizz <mohafizz@gmail.com>
2018-02-28 00:41:56 +0800

    Exercises completed but commented out

--- 3-objects.js
+++ 3-objects.js
@@ -2,13 +2,20 @@ var assertEquals = require("./test-helper");
 
 /* ---------------------- EXERCISE 1 ---------------------- */
 // Define a function to list the properties of a JavaScript object. Go to the editor
+// var output = "";
+// function listProperties(anyObject) {
+//   for (key in anyObject) {
+//     output += key;
+//   }
+//   return output;
+// }
+// assertEquals(listProperties({ name: "Tom Hanks" }), "name");
+// assertEquals(
+//   listProperties({ name: "Tom Hanks", age: 50, rollno: 12 }),
+//   "name age rollno"
+// );
 
-function listProperties() {}
-
-// assertEquals(listProperties({name: 'Tom Hanks'}), 'name')
-// assertEquals(listProperties({name: 'Tom Hanks', age: 50, rollno: 12}), 'name age rollno')
-
-// Sample Output: name,sclass,rollno
+//Sample Output: name,sclass,rollno
 
 /* ---------------------- EXERCISE 2 ---------------------- */
 
@@ -31,24 +38,60 @@ var books = [
 ];
 
 // 2.1 Define a function listTitles(booksArray) that takes in an array of book objects and returns an array of titles (strings)
-function listTitles(booksArray) {}
+// var output = [];
+// function listTitles(booksArray) {
+//   for(key of booksArray){
+//     output.push(key.title);
+//   }
+// }
+// listTitles(books);
+// console.log(output);
 
 // assertEquals(listTitles(books), ['The Road Ahead', 'Lord of the Rings', 'Harry Potter: The Prisoner of Azkaban'])
 
 // 2.2 Define a function listAuthors(booksArray) that takes in an array of book objects and returns an array of authors (strings)
 
-function listAuthors(booksArray) {}
+
+// var output = [];
+// function listAuthors(booksArray) {
+//   for(key of booksArray){
+//     output.push(key.author);
+//   }
+// }
+// listAuthors(books);
+// console.log(output);
 
 // assertEquals(listTitles(books), ['Bill Gates', 'JRR Tolkkien', 'JK Rowling'])
 
 // 2.3 Define a more general function listValues(booksArray, key) that takes in an array of book objects and returns an array of authors (strings)
-
-function listValues(booksArray, key) {}
+// var output = [];
+// function listValues(booksArray, key) {
+//     for(keys of booksArray){
+//       if(key=="author"){
+//       output.push(keys.author);
+//       } if(key=="title"){
+//         output.push(keys.title);
+//       } 
+//     }
+//   }
+//   listValues(books, "title");
+//   console.log(output);
 
 // assertEquals(listValues(books, 'author'), ['Bill Gates', 'JRR Tolkkien', 'JK Rowling'])
 // assertEquals(listValues(books, 'title'), ['The Road Ahead', 'Lord of the Rings', 'Harry Potter: The Prisoner of Azkaban'])
 
 // 2.4 Define a function getAvailableBooks(booksArray) that returns a list of available books
+
+// var output = [];
+// function listAvailable(booksArray) {
+//     for(keys of booksArray){
+//       if(keys.isAvailable){
+//       output.push(keys.author);
+//       } 
+//     }
+//   }
+//   listAvailable(books);
+//   console.log(output);
 
 // assertEquals(getAvailableBooks(books), ['The Road Ahead', 'Lord of the Rings'])
 
@@ -59,6 +102,23 @@ function listValues(booksArray, key) {}
     - makeSound() // return 'meow!'
     - introduceSelf() // return 'meow! my name is fluffy'
 */
+
+// var fluffycat = {
+//   name: 'fluffy',
+//   type: 'pet',
+//   makesound: function(){
+//     //return 'meow!';
+//     console.log('meow!');
+//   },
+//   introduceSelf(){
+//     console.log(this.makesound())
+//     console.log("my name is "+this.name)
+//   }
+// };
+
+// // console.log(fluffycat);
+// fluffycat.makesound();
+// fluffycat.introduceSelf();
 
 // assertEquals(fluffycat.name, "fluffy");
 // assertEquals(fluffycat.type, "pet");
