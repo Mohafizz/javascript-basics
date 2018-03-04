c2c25b7eaf01ff18960bed7ffb28ce08c3ec88e4
mohafizz <mohafizz@gmail.com>
2018-03-05 01:12:44 +0800

    uploaded solutions for Exercise 2

--- 3-objects.js
+++ 3-objects.js
@@ -3,96 +3,107 @@ var assertEquals = require("./test-helper");
 /* ---------------------- EXERCISE 1 ---------------------- */
 // Define a function to list the properties of a JavaScript object. Go to the editor
 
-function listProperties(anyObject) {
-  var output = [];
-  for (var key in anyObject) {
-    output.push(key); //for accessing keys
-  }
-  var finalOutput = output.join(" ");
-  return finalOutput;
-}
-assertEquals(listProperties({ name: "Tom Hanks" }), "name");
-assertEquals(
-  listProperties({ name: "Tom Hanks", age: 50, rollno: 12 }),
-  "name age rollno"
-);
+// function listProperties(anyObject) {
+//   var output = [];
+//   for (var key in anyObject) {
+//     output.push(key); //for accessing keys
+//   }
+//   var finalOutput = output.join(" ");
+//   return finalOutput;
+// }
+// assertEquals(listProperties({ name: "Tom Hanks" }), "name");
+// assertEquals(
+//   listProperties({ name: "Tom Hanks", age: 50, rollno: 12 }),
+//   "name age rollno"
+// );
 
 //Sample Output: name,sclass,rollno
 
 /* ---------------------- EXERCISE 2 ---------------------- */
 
-// var books = [
-//   {
-//     author: "Bill Gates",
-//     title: "The Road Ahead",
-//     isAvailable: true
-//   },
-//   {
-//     author: "JRR Tolkkien",
-//     title: "Lord of the Rings",
-//     isAvailable: true
-//   },
-//   {
-//     author: "JK Rowling",
-//     title: "Harry Potter: The Prisoner of Azkaban",
-//     isAvailable: false
-//   }
-// ];
+var books = [
+  {
+    author: "Bill Gates",
+    title: "The Road Ahead",
+    isAvailable: true
+  },
+  {
+    author: "JRR Tolkkien",
+    title: "Lord of the Rings",
+    isAvailable: true
+  },
+  {
+    author: "JK Rowling",
+    title: "Harry Potter: The Prisoner of Azkaban",
+    isAvailable: false
+  }
+];
 
 // 2.1 Define a function listTitles(booksArray) that takes in an array of book objects and returns an array of titles (strings)
-// var output = [];
+
 // function listTitles(booksArray) {
-//   for(key of booksArray){
+//   var output = [];
+//   for (var key of booksArray) {
 //     output.push(key.title);
 //   }
+//   return output;
 // }
 // listTitles(books);
-// console.log(output);
 
-// assertEquals(listTitles(books), ['The Road Ahead', 'Lord of the Rings', 'Harry Potter: The Prisoner of Azkaban'])
+// assertEquals(listTitles(books), [
+//   "The Road Ahead",
+//   "Lord of the Rings",
+//   "Harry Potter: The Prisoner of Azkaban"
+// ]);
 
 // 2.2 Define a function listAuthors(booksArray) that takes in an array of book objects and returns an array of authors (strings)
 
-// var output = [];
 // function listAuthors(booksArray) {
-//   for(key of booksArray){
+//   var output = [];
+//   for (key of booksArray) {
 //     output.push(key.author);
 //   }
+//   return output;
 // }
 // listAuthors(books);
-// console.log(output);
 
-// assertEquals(listTitles(books), ['Bill Gates', 'JRR Tolkkien', 'JK Rowling'])
+// assertEquals(listAuthors(books), ["Bill Gates", "JRR Tolkkien", "JK Rowling"]);
 
 // 2.3 Define a more general function listValues(booksArray, key) that takes in an array of book objects and returns an array of authors (strings)
-// var output = [];
-// function listValues(booksArray, key) {
-//     for(keys of booksArray){
-//       if(key=="author"){
-//       output.push(keys.author);
-//       } if(key=="title"){
-//         output.push(keys.title);
-//       }
-//     }
-//   }
-//   listValues(books, "title");
-//   console.log(output);
 
-// assertEquals(listValues(books, 'author'), ['Bill Gates', 'JRR Tolkkien', 'JK Rowling'])
-// assertEquals(listValues(books, 'title'), ['The Road Ahead', 'Lord of the Rings', 'Harry Potter: The Prisoner of Azkaban'])
+// function listValues(booksArray, key) {
+//   var output = [];
+//   for (keys of booksArray) {
+//     output.push(keys[key]);
+//   }
+//   return output;
+// }
+// // listValues(books, "author");
+
+// assertEquals(listValues(books, "author"), [
+//   "Bill Gates",
+//   "JRR Tolkkien",
+//   "JK Rowling"
+// ]);
+// assertEquals(listValues(books, "title"), [
+//   "The Road Ahead",
+//   "Lord of the Rings",
+//   "Harry Potter: The Prisoner of Azkaban"
+// ]);
 
 // 2.4 Define a function getAvailableBooks(booksArray) that returns a list of available books
 
-// var output = [];
-// function listAvailable(booksArray) {
-//     for(keys of booksArray){
-//       if(keys.isAvailable){
-//       output.push(keys.author);
-//       }
+// function getAvailableBooks(booksArray) {
+//   var output = [];
+//   for (keys of booksArray) {
+//     if (keys.isAvailable) {
+//       output.push(keys);
+//       console.log("output: ", output);
 //     }
 //   }
-//   listAvailable(books);
-//   console.log(output);
+// }
+// getAvailableBooks(books);
+// console.log(output);
 
 // assertEquals(getAvailableBooks(books), ['The Road Ahead', 'Lord of the Rings'])
 
